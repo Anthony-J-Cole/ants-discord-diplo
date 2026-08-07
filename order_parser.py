f4d4b9e1772005d337672db7f4914cc4f0c18bac
Anthony Cole <coolcole@outlook.co.nz>
2026-08-07 15:44:53 +1200

    Order parser format

---
+++ order_parser.py
@@ -0,0 +1,18 @@
+# Current idea is to pass in orders in the form that is on the DATC
+
+# /order A PAR                  | Hold
+# /order A PAR H                | Hold
+# /order A PAR - BUR            | Move
+# /order A GAS S A PAR - BUR    | Support Move
+# /order A GAS S A PAR          | Support Hold
+# /order F MAO C A POR - MAR    | Convoy
+
+# Retreat Phase
+# /order A BUR - MAR            | Retreat
+# /order A BUR R MAR            | Retreat
+# /order A BUR DISBAND
+
+# Build Phase:
+# /order BUILD A PAR            | Build
+# /order BUILD F MAR/nc         | Build
+# /order DISBAND A MAR          | Disband
